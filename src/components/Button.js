3cf0aaa12e9be2b0bcf9af9bf62c74f27ae247de
Andrew <gskll@protonmail.com>
2020-12-05 14:43:40 +0100

    feat: add color context to show multiple contexts

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,24 @@
 import React from "react";
 
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
   renderButtonText = (value) => {
     return value === "english" ? "Submit" : "Envoyer";
   };
+
   render() {
     return (
-      <button className="ui button primary">
-        <LanguageContext.Consumer>
-          {(v) => this.renderButtonText(v)}
-        </LanguageContext.Consumer>
-      </button>
+      <ColorContext.Consumer>
+        {(color) => (
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {(v) => this.renderButtonText(v)}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
     );
   }
 }
